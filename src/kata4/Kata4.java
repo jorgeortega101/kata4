06f0a6ca73ac17050333c9f52db6b816039a4a2a
jorgeortega <jorgeortega@10.230.173.74>
2016-11-04 11:02:10 +0000

    Kata4 versión 2

--- src/kata4/Kata4.java
+++ src/kata4/Kata4.java
@@ -6,9 +6,17 @@ public class Kata4 {
 
     public static void main(String[] args) {
         File file = new File("/Users/jorgeortega/Documents/ULPGC/3º Año/Is2");
-        String[] names=file.list();
-        for (String name: names){
-            System.out.println(name);
+        print(file.listFiles(), "");
+    }
+    
+    private static void print(File[] files, String indent){
+        if(files == null) return;
+        for(File file:files){
+            System.out.println(indent + 
+                    (file.isDirectory() ? "+" : "-")+
+                    file.getName());
+            if(!file.isDirectory() || file.isHidden())continue;
+            print(file.listFiles(), " ");
         }
     }
     
